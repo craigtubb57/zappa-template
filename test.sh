ec3319e5a7085635118167698938ce39903f94d5
Craig Tubb <craig.tubb.57@gmail.com>
2023-09-18 17:55:18 +0100

    Fix permissions on bash files

--- test.sh
+++
@@ -1,8 +0,0 @@
-# #!/bin/bash
-#
-# basename `git rev-parse --show-toplevel`
-#
-# project_name=$(basename `git rev-parse --show-toplevel`)
-# echo $project_name
-#
-# echo "lower" | tr [:lower:] [:upper:]
